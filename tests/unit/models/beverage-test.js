af57127430237fabfc2e1710089a7254b1a0f655
AzureAD\SantiagoGonzálezEraz <serazo@customerville.com>
2021-12-18 18:06:58 +0100

    beverage model & test

---
+++ tests/unit/models/beverage-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Model | beverage', function (hooks) {
+  setupTest(hooks);
+
+  test('it has the right attributes', function (assert) {
+    let store = this.owner.lookup('service:store');
+    let beverage = store.createRecord('beverage', {
+      id: 'GR1',
+      name: 'Green tea',
+      price: 3.11
+    });
+    
+    assert.equal(beverage.name, 'Green tea');
+    assert.equal(beverage.price, 3.11);
+  });
+});

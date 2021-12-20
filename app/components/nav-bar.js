c09a70c7d9d73593879f63fd24c5e569a35bd57f
AzureAD\SantiagoGonzálezEraz <serazo@customerville.com>
2021-12-21 00:45:42 +0100

    cart logic done

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -3,4 +3,11 @@ import { inject as service } from '@ember/service';
 
 export default class NavBarComponent extends Component {
   @service ('shopping-cart') cart;
+
+  get itemCount() {
+    console.log( this.cart.itemList.length);
+    return this.cart.itemList.reduce((total, item) => {
+      return total += item.count;
+    }, 0);
+  }
 }
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -1,11 +1,36 @@
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
+class Item {
+  @tracked count;
+
+  name;
+  image;
+  price;
+
+  constructor(item) {
+    this.count = item.count;
+    this.name = item.name;
+    this.image = item.image;
+    this.price = item.price;
+  }
+}
+
 export default class ShoppingCartService extends Service {
   @tracked itemList = [];
 
   addItem(item) {
-    this.itemList = [...this.itemList, item];
-    console.log(item);
+    const existingItem = this.itemList.find(({ name }) => {
+      return name === item.name;
+    });
+
+    if (existingItem) {
+      existingItem.count += 1;
+    } else {
+      this.itemList = [...this.itemList, new Item ({
+        ...item,
+        count: 1,
+      })];
+    }
   }
 }

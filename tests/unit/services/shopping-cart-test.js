0756bb5cfc70c58f44b86df580418ee949e60a87
AzureAD\SantiagoGonzálezEraz <serazo@customerville.com>
2021-12-21 22:55:46 +0100

    shopping-cart service test done

--- tests/unit/services/shopping-cart-test.js
+++ tests/unit/services/shopping-cart-test.js
@@ -4,9 +4,28 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Service | shopping-cart', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it exists', function (assert) {
-    let service = this.owner.lookup('service:shopping-cart');
-    assert.ok(service);
-  });
+  test('addItem method works', function(assert) {
+    const service = this.owner.lookup('service:shopping-cart');
+    assert.equal(service.itemList.length, 0, 'item list is empty');
+    service.addItem({});
+    assert.equal(service.itemList.length, 1, 'item list size increase by 1');
+  })
+
+  test('remove method works', function(assert) {
+    const service = this.owner.lookup('service:shopping-cart');
+    assert.equal(service.itemList.length, 0, 'item list is empty');
+    service.addItem({});
+    assert.equal(service.itemList.length, 1, 'item list size increase by 1');
+    service.remove({});
+    assert.equal(service.itemList.length, 0, 'item list size decrease by 1');
+  })
+
+  test('removeAll method works', function(assert) {
+    const service = this.owner.lookup('service:shopping-cart');
+    assert.equal(service.itemList.length, 0, 'item list is empty');
+    service.addItem({});
+    assert.equal(service.itemList.length, 1, 'item list size increase by 1');
+    service.removeAll();
+    assert.equal(service.itemList.length, 0, 'item list it\'s empty');
+  })
 });
